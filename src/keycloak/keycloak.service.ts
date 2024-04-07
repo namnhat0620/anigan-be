9dc7212bb62e81bced047c3f117b7eb0aceb5fcc
namnhat0620 <94464940+namnhat0620@users.noreply.github.com>
2024-04-07 18:05:41 +0700

    fix req login keycloak

--- src/keycloak/keycloak.service.ts
+++ src/keycloak/keycloak.service.ts
@@ -1,32 +1,27 @@
 import { Injectable } from '@nestjs/common';
-import axios from 'axios';
-import https from 'https';
 import { LoginDto } from './dto/login.dto';
+import axios from 'axios';
 
 @Injectable()
 export class KeycloakService {
-
   async login(loginDto: LoginDto) {
-    const url = `${process.env.KEYCLOAK_URL}${process.env.KEYCLOAK_PREFIX}/protocol/openid-connect/token`;
+    const url = `${process.env.KEYCLOAK_URL}${process.env.KEYCLOAK_PREFIX}protocol/openid-connect/token`;
+    console.log({ url });
 
     const { username, password } = loginDto;
     try {
-      const response = await axios.post(url, null, {
-        params: {
-          client_id: 'anigan-be',
-          client_secret: 'i2UrEL07qUBU2sEQSTIDQ0gUuOaC9WtF',
-          grant_type: 'password',
-          username,
-          password,
-        },
+      const response = await axios.post(url, {
+        client_id: 'anigan-be',
+        client_secret: 'i2UrEL07qUBU2sEQSTIDQ0gUuOaC9WtF',
+        grant_type: 'password',
+        username,
+        password,
+      }, {
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
         }
       });
-
-      console.log({ data: response });
-
-      return response;
+      return response.data
     } catch (error) {
       // Handle error
       console.error('Error:', error.response);
